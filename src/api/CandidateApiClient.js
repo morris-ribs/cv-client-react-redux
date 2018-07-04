fe2bd42f43bf4dd37c5fc3a1fce2b27273fe2133
morris-ribs <maumribeiro@gmail.com>
2018-07-04 13:45:57 +0200

    Connecting to heroku

--- src/api/CandidateApiClient.js
+++ src/api/CandidateApiClient.js
@@ -2,7 +2,7 @@ class CandidateApiClient {
   static getCandidate(candidateId) {
     console.log(candidateId);
     // here we do a simple call to the dummy server and retrieve the response as JSON 
-    return fetch(`http://localhost:4000/${candidateId}`).then(response => response.json());
+    return fetch(`https://cv-server-rest-go.herokuapp.com/candidate/${candidateId}`).then(response => response.json());
   }
 }
 
